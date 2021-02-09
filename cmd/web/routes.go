f3f533a1026e691d48aa44739b7325db3869d5f7
valorvig <valor.vignette@gmail.com>
2021-02-09 15:48:44 +0700

    Add images, add a pipeline to home.page.tmpl, and add fileSever to routes.go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,5 +20,10 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// the img won't know how to get to the folder "/static/images/house.jpg"
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer)) // strip "/static" with "./static"
+
 	return mux
 }
