5dfbb20778577c29ea8e2bb0d38be4f3329bfa5b
valorvig <valor.vignette@gmail.com>
2021-03-08 15:05:56 +0700

    Add soda sql migration up/down for table rooms

---
+++ migrations/20210308070409_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Gerneral''s Quarter','2020-11-18 00:00:00.000','2020-11-18 00:00:00.000'),
+	 ('Major''s Suite','2020-11-18 00:00:00.000','2020-11-18 00:00:00.000');
