faca9240311907f877b1721e614d911f3e9d48a2
valorvig <valor.vignette@gmail.com>
2021-03-21 21:45:18 +0700

    Add the reservation map to the calendar page with displayed R symbols that link to each reservation for more info. Add cancel button that brings back to the current month page.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -862,7 +862,7 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 				}
 			} else {
 				// it's a block
-				blockMap[y.StartDate.Format("2006-01-2")] = y.RestrictionID
+				blockMap[y.StartDate.Format("2006-01-2")] = y.RestrictionID // store ID
 			}
 		}
 
